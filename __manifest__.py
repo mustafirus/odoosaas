720e12fd2ef47af4d8d0fda11300e3d0f9ef0374
Alex <1stpaninalexey@gmail.com>
2019-03-24 02:33:39 +0200

    access rights ir.model.access.csv

--- __manifest__.py
+++ __manifest__.py
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/database.xml',
         'views/templates.xml',
     ],
